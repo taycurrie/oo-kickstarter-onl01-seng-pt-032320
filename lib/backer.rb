8536f747999ff5e38bc5d064b7a3438e7a3c5c12
sunny-tier-6236 <taycurrie@gmail.com>
2020-04-22 18:56:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
-class Project
-  attr_reader :title, :backers
+class Backer
+  attr_reader :name, :backed_projects
 
-  def initialize(title)
-    @title = title
-    @backers = []
+  def initialize(name)
+    @name = name
+    @backed_projects = []
   end
 
-  def add_backer(backer)
-    backers << backer
-    backer.backed_projects << self
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
   end
 
 end
